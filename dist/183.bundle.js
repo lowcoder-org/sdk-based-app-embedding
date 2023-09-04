4d5730405bb492f4d4a799aaabfcb81f30fe0786
Aqib Mirza <aaron.smith@nninjatechnolabs.com>
2023-09-04 13:07:18 +0530

    fix: lowcoder comp issue

---
+++ dist/183.bundle.js
@@ -0,0 +1,18 @@
+"use strict";(self.webpackChunklowcoder_sdk_embedding=self.webpackChunklowcoder_sdk_embedding||[]).push([[183],{183:(e,t,o)=>{o.r(t),o.d(t,{DateMobileUIView:()=>s,DateRangeMobileUIView:()=>l});var n=o(371);o(294),o(935);const a=async e=>{const t=(await Promise.all([o.e(950),o.e(831)]).then(o.bind(o,831))).default,a=(0,n.h)(e.minDate,n.k),d=(0,n.h)(e.maxDate,n.k),{disabledHours:i,disabledMinutes:s,disabledSeconds:l}=e.disabledTime();t.prompt({getContainer:()=>document.querySelector(`#${n.C}`)||document.body,mouseWheel:!0,destroyOnClose:!0,closeOnMaskClick:!0,min:a.isValid()?a.toDate():void 0,max:d.isValid()?d.toDate():void 0,precision:e.showTime?"second":"day",defaultValue:e.value?e.value.toDate():void 0,filter:{hour:e=>!i().includes(e),minute:(e,{date:t})=>!s(t.getHours()).includes(e),second:(e,{date:t})=>!l(t.getHours(),t.getMinutes()).includes(e)},onConfirm:t=>{const o=(0,n.h)(t);e.onChange(o)},onClose:e.onBlur}),e.onFocus()},d=n.s.div`
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 8px;
+  background-color: #ffffff;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 1px solid #d7d9e0;
+  ${e=>e.$style&&(0,n.e)(e.$style)}
+`,i=n.s.div`
+  overflow: hidden;
+  white-space: nowrap;
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+`,s=e=>(0,n.a)(d,{ref:e.viewRef,$style:e.$style,onClick:()=>a(e),children:[(0,n.j)(i,{children:e.value?e.value.format(e.format||(e.showTime?n.D:n.f)):(0,n.t)("date.placeholder")}),e.suffixIcon]}),l=e=>(0,n.a)(d,{ref:e.viewRef,$style:e.$style,children:[(0,n.j)(i,{onClick:()=>a({...e,value:e.start,onChange:t=>e.onChange(t,e.end)}),children:e.start?e.start.format(e.format||(e.showTime?n.D:n.f)):(0,n.t)("date.startDate")}),(0,n.j)(n.S,{}),(0,n.j)(i,{onClick:()=>a({...e,value:e.end,onChange:t=>e.onChange(e.start,t)}),children:e.end?e.end.format(e.format||(e.showTime?n.D:n.f)):(0,n.t)("date.endDate")}),e.suffixIcon]})}}]);
---
+++ dist/603.bundle.js
@@ -0,0 +1,19 @@
+"use strict";(self.webpackChunklowcoder_sdk_embedding=self.webpackChunklowcoder_sdk_embedding||[]).push([[603],{603:(e,n,l)=>{l.r(n),l.d(n,{TimeMobileUIView:()=>m,TimeRangeMobileUIView:()=>f});var t=l(950),o=l(371);l(700),l(294),l(935),t.c&&document.addEventListener("touchstart",(()=>{}),!0);const r=(e=1)=>[...o.n.range(0,24,e).map((e=>{const n=(e<10?"0":"")+e;return{label:n,value:n}}))],a=(e=1)=>[...o.n.range(0,12,e).map((e=>{const n=(e<10?"0":"")+e;return{label:n,value:n}}))],s=(e=1)=>o.n.range(0,60,e).map((e=>{const n=(e<10?"0":"")+e;return{label:n,value:n}})),i=[{label:"AM",value:"am"},{label:"PM",value:"pm"}],u=e=>{const{disabledHours:n,disabledMinutes:l,disabledSeconds:u}=e.disabledTime();t.P.prompt({getContainer:()=>document.querySelector(`#${o.C}`)||document.body,mouseWheel:!0,destroyOnClose:!0,closeOnMaskClick:!0,columns:t=>{const d=(0,o.h)(t.join(":"),o.l);return[(e.use12Hours?a:r)(e.hourStep).filter((({label:e,value:l})=>!n().includes(Number(l)+("pm"===t[3]?12:0)))),s(e.minuteStep).filter((({label:e,value:n})=>!l(d.hour()).includes(Number(n)))),s(e.secondStep).filter((({label:e,value:n})=>!u(d.hour(),d.minute()).includes(Number(n)))),...e.use12Hours?[i]:[]]},defaultValue:e.value?e.value.format(e.use12Hours?o.m:o.T).split(":"):void 0,onConfirm:n=>{const l=(0,o.h)(n.join(":"),o.l);e.onChange(l)},onClose:e.onBlur}),e.onFocus()},d=o.s.div`
+  height: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 8px;
+  background-color: #ffffff;
+  cursor: pointer;
+  border-radius: 4px;
+  border: 1px solid #d7d9e0;
+
+  ${e=>e.$style&&(0,o.e)(e.$style)}
+`,c=o.s.div`
+  overflow: hidden;
+  white-space: nowrap;
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+`,m=e=>(0,o.a)(d,{ref:e.viewRef,$style:e.$style,onClick:()=>{u(e)},children:[(0,o.j)(c,{children:e.value?e.value.format(e.format||o.T):(0,o.t)("time.placeholder")}),e.suffixIcon]}),f=e=>(0,o.a)(d,{ref:e.viewRef,$style:e.$style,children:[(0,o.j)(c,{onClick:()=>u({...e,value:e.start,onChange:n=>e.onChange(n,e.end)}),children:e.start?e.start.format(e.format||o.T):(0,o.t)("time.startTime")}),(0,o.j)(o.S,{}),(0,o.j)(c,{onClick:()=>u({...e,value:e.end,onChange:n=>e.onChange(e.start,n)}),children:e.end?e.end.format(e.format||o.T):(0,o.t)("time.endTime")}),e.suffixIcon]})}}]);
